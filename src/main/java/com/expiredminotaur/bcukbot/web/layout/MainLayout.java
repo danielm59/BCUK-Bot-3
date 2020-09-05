f4f2fe7079b10ba44e4957a80737aa74ae025c67
Daniel M <danielm59@gmail.com>
2020-09-05 10:53:11 +0100

    database view done

--- src/main/java/com/expiredminotaur/bcukbot/web/layout/MainLayout.java
+++ src/main/java/com/expiredminotaur/bcukbot/web/layout/MainLayout.java
@@ -7,6 +7,7 @@ import com.expiredminotaur.bcukbot.web.view.collection.ClipView;
 import com.expiredminotaur.bcukbot.web.view.collection.JokeView;
 import com.expiredminotaur.bcukbot.web.view.collection.QuoteView;
 import com.expiredminotaur.bcukbot.web.view.settings.CountersView;
+import com.expiredminotaur.bcukbot.web.view.settings.DatabaseView;
 import com.expiredminotaur.bcukbot.web.view.settings.MusicSettingsView;
 import com.expiredminotaur.bcukbot.web.view.settings.SFXView;
 import com.expiredminotaur.bcukbot.web.view.settings.StreamAnnouncementsView;
@@ -44,7 +45,7 @@ public class MainLayout extends AppLayout
 
         menu.addItem("Home", e -> UI.getCurrent().navigate(MainView.class));
         MenuItem bots = menu.addItem("Bots");
-        MenuItem Music = menu.addItem("Music", e -> UI.getCurrent().getPage().setLocation("/music"));
+        menu.addItem("Music", e -> UI.getCurrent().getPage().setLocation("/music"));
         MenuItem collections = menu.addItem("Collections");
         MenuItem settings = menu.addItem("Settings");
         menu.addItem("Logout", e -> UI.getCurrent().getPage().setLocation("/logout"));
@@ -63,6 +64,7 @@ public class MainLayout extends AppLayout
         settingSubMenu.addItem("Counters", e -> UI.getCurrent().navigate(CountersView.class));
         settingSubMenu.addItem("SFX", e -> UI.getCurrent().navigate(SFXView.class));
         settingSubMenu.addItem("Music", e -> UI.getCurrent().navigate(MusicSettingsView.class));
+        settingSubMenu.addItem("Database", e -> UI.getCurrent().navigate(DatabaseView.class));
 
         addToNavbar(true, logo, menu);
 
--- src/main/java/com/expiredminotaur/bcukbot/web/view/settings/DatabaseView.java
+++ src/main/java/com/expiredminotaur/bcukbot/web/view/settings/DatabaseView.java
@@ -1,12 +1,88 @@
 package com.expiredminotaur.bcukbot.web.view.settings;
 
+import com.expiredminotaur.bcukbot.sql.user.User;
+import com.expiredminotaur.bcukbot.sql.user.UserRepository;
 import com.expiredminotaur.bcukbot.web.layout.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.data.converter.StringToLongConverter;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
 
 @Route(value = "settings/database", layout = MainLayout.class)
 public class DatabaseView extends VerticalLayout
 {
-    //TODO add a way to register a user (enter discordID/reference)
-    //TODO add a way to invalidate all caches
+    @Autowired
+    private CacheManager cacheManager;
+
+    public DatabaseView(@Autowired UserRepository users, @Autowired CacheManager cacheManager)
+    {
+        Button registerUserButton = new Button("Register User", e -> registerUser(users));
+        Button resetCacheButton = new Button("Reset Cache", e -> resetCache(cacheManager));
+        add(registerUserButton, resetCacheButton);
+    }
+
+    private void registerUser(UserRepository users)
+    {
+        Dialog dialog = new Dialog();
+        FormLayout layout = new FormLayout();
+        Binder<User> binder = new Binder<>(User.class);
+
+        TextField discordID = new TextField();
+        binder.forField(discordID).withConverter(new StringToLongConverter("Invalid Discord ID"))
+                .bind(User::getDiscordId, User::setDiscordId);
+        layout.addFormItem(discordID, "Discord ID/Reference");
+
+        HorizontalLayout buttons = new HorizontalLayout();
+        Button save = new Button("Save", e ->
+        {
+            try
+            {
+                if (binder.isValid())
+                {
+                    User user = new User(-1L);
+                    binder.writeBean(user);
+                    if (user.getDiscordId() != null)
+                        if (users.findById(user.getDiscordId()).isPresent())
+                        {
+                            discordID.setErrorMessage("User already registered");
+                            discordID.setInvalid(true);
+                        } else
+                            users.save(user);
+                    else
+                    {
+                        discordID.setErrorMessage("Required");
+                        discordID.setInvalid(true);
+                    }
+                }
+            } catch (ValidationException ex)
+            {
+                ex.printStackTrace();
+            }
+        });
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        Button cancel = new Button("Cancel", e -> dialog.close());
+        cancel.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_ERROR);
+        buttons.add(save, cancel);
+
+        dialog.add(layout, buttons);
+        dialog.setCloseOnOutsideClick(false);
+        dialog.open();
+    }
+
+    private void resetCache(CacheManager cacheManager)
+    {
+        for (String name : cacheManager.getCacheNames())
+        {
+            cacheManager.getCache(name).clear();
+        }
+    }
 }
