b59a1713f2a7cb1f3b59075ea5bd4dcf7fb73fbc
Daniel M <danielm59@gmail.com>
2020-09-24 21:11:14 +0100

    cleanup of Collection View code

--- src/main/java/com/expiredminotaur/bcukbot/web/view/collection/JokeView.java
+++ src/main/java/com/expiredminotaur/bcukbot/web/view/collection/JokeView.java
@@ -4,96 +4,17 @@ import com.expiredminotaur.bcukbot.sql.collection.joke.Joke;
 import com.expiredminotaur.bcukbot.sql.collection.joke.JokeRepository;
 import com.expiredminotaur.bcukbot.web.layout.MainLayout;
 import com.expiredminotaur.bcukbot.web.security.UserTools;
-import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
-import com.vaadin.flow.data.binder.ValidationException;
-import com.vaadin.flow.data.renderer.ComponentRenderer;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.annotation.PostConstruct;
-
 @Route(value = "jokes", layout = MainLayout.class)
-public class JokeView extends VerticalLayout
+public class JokeView extends CollectionView<Joke>
 {
-    private final Grid<Joke> jokeGrid = new Grid<>(Joke.class);
-    @Autowired
-    private JokeRepository jokes;
-
-    public JokeView(@Autowired UserTools userTools)
+    public JokeView(@Autowired UserTools userTools, @Autowired JokeRepository repository)
     {
-        setSizeFull();
-        H2 header = new H2("Jokes");
-        jokeGrid.setColumns("id", "joke", "source", "date");
-        jokeGrid.setSizeFull();
-
-        if (userTools.isCurrentUserAdmin())
-        {
-            jokeGrid.addColumn(new ComponentRenderer<>(joke -> new Button("Edit", e -> edit(joke))))
-                    .setHeader("Edit")
-                    .setFlexGrow(0);
-        }
-
-        jokeGrid.getColumns().forEach(c -> c.setAutoWidth(true));
-        jokeGrid.recalculateColumnWidths();
-        add(header, jokeGrid);
-    }
-
-    @PostConstruct
-    private void initData()
-    {
-        jokeGrid.setItems(jokes.findAll());
-    }
-
-    private void edit(Joke joke)
-    {
-        Dialog editDialog = new Dialog();
-        editDialog.setWidth("60%");
-        editDialog.setCloseOnOutsideClick(false);
-        FormLayout layout = new FormLayout();
-        layout.setResponsiveSteps(
-                new FormLayout.ResponsiveStep("0", 1, FormLayout.ResponsiveStep.LabelsPosition.TOP),
-                new FormLayout.ResponsiveStep("600px", 1, FormLayout.ResponsiveStep.LabelsPosition.ASIDE));
-
-        TextField jokeField = new TextField();
-        jokeField.setWidthFull();
-
-        Binder<Joke> binder = new Binder<>(Joke.class);
-        binder.forField(jokeField).bind("joke");
-        layout.addFormItem(jokeField, "Joke");
-
-        HorizontalLayout buttons = new HorizontalLayout();
-        Button save = new Button("Save", e ->
-        {
-            try
-            {
-                binder.writeBean(joke);
-                jokes.save(joke);
-                jokeGrid.getDataProvider().refreshItem(joke);
-                editDialog.close();
-
-            } catch (ValidationException ex)
-            {
-                ex.printStackTrace();
-            }
-        });
-        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        Button cancel = new Button("Cancel", e -> editDialog.close());
-        cancel.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_ERROR);
-        buttons.add(save, cancel);
-        buttons.setJustifyContentMode(JustifyContentMode.END);
-
-        editDialog.add(layout, buttons);
-
-        binder.readBean(joke);
-        editDialog.open();
+        super(userTools, repository, new Grid<>(Joke.class), new Binder<>(Joke.class));
+        setup("Jokes", "joke", "Joke");
     }
 }
