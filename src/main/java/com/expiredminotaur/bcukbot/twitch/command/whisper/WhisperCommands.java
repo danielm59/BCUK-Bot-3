7d3ea26e8dcf79f854de58edb890e88623154d25
Daniel M <danielm59@gmail.com>
2021-03-03 18:48:18 +0000

    whisper commands now use the ones used in discord to remove duplicated code

--- src/main/java/com/expiredminotaur/bcukbot/twitch/command/whisper/WhisperCommands.java
+++ src/main/java/com/expiredminotaur/bcukbot/twitch/command/whisper/WhisperCommands.java
@@ -1,40 +1,19 @@
 package com.expiredminotaur.bcukbot.twitch.command.whisper;
 
 import com.expiredminotaur.bcukbot.command.Commands;
-import com.expiredminotaur.bcukbot.discord.music.MusicHandler;
+import com.expiredminotaur.bcukbot.discord.command.DiscordMusicCommands;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 @Component
 public class WhisperCommands extends Commands<WhisperCommand, WhisperCommandEvent>
 {
     @Autowired
-    @Lazy
-    private MusicHandler musicHandler;
+    private DiscordMusicCommands discordMusicCommands;
 
     public WhisperCommands()
     {
-        commands.put("!volume", new WhisperCommand(this::volume));
-        commands.put("!pause", new WhisperCommand(e -> musicHandler.togglePause(e)));
-    }
-
-    private Mono<Void> volume(WhisperCommandEvent event)
-    {
-        String content = event.getFinalMessage();
-
-        if (!content.isEmpty())
-        {
-            String[] args = content.split(" ");
-            if (args.length > 1)
-            {
-                return musicHandler.setVolume(event, args[1]);
-            } else
-            {
-                return musicHandler.getVolume(event);
-            }
-        }
-        return event.empty();
+        commands.put("!volume", new WhisperCommand(discordMusicCommands::volume));
+        commands.put("!pause", new WhisperCommand(discordMusicCommands::pause));
     }
 }
