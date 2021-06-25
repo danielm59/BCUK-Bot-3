a4247abc2d64275f5fadd05ed5c371ece256de80
ExpiredMinotaur <1735367+ExpiredMinotaur@users.noreply.github.com>
2021-06-25 16:20:05 +0100

    remove temp fix for spring (for when 2.5.1 releases) (#96)
    * remove temp fix for spring (for when 1.5.1 releases)
    
    * fixed config folder delete code
    
    * Just giving commands are not generic for any charity event
    
    * Vaadin update
    
    * Bump spring-boot-starter-parent from 2.5.0 to 2.5.2 (#103)
    
    Bumps [spring-boot-starter-parent](https://github.com/spring-projects/spring-boot) from 2.5.0 to 2.5.2.
    - [Release notes](https://github.com/spring-projects/spring-boot/releases)
    - [Commits](https://github.com/spring-projects/spring-boot/compare/v2.5.0...v2.5.2)
    
    ---
    updated-dependencies:
    - dependency-name: org.springframework.boot:spring-boot-starter-parent
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/main/java/com/expiredminotaur/bcukbot/Application.java
+++ src/main/java/com/expiredminotaur/bcukbot/Application.java
@@ -1,38 +1,19 @@
 package com.expiredminotaur.bcukbot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.util.FileSystemUtils;
-
-import java.io.File;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 @EnableCaching
 @EnableScheduling
 public class Application extends SpringBootServletInitializer
 {
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args)
     {
-        removeConfigFolder();
         SpringApplication.run(Application.class, args);
-    }
-
-    private static void removeConfigFolder()
-    {
-        String directoryPath = "./config";
-        File file = new File(directoryPath);
-
-        if (!FileSystemUtils.deleteRecursively(file))
-        {
-            logger.warn("Problem occurs when deleting the directory : " + directoryPath);
-        }
     }
 }
